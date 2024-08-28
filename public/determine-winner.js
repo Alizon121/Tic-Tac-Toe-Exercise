1911da4ce429e226026471f5490f50682dd8ee97
AlanDeleon88 <19deleon88@gmail.com>
2024-08-28 15:01:09 -0700

    pushing up branch for reference

--- public/determine-winner.js
+++ public/determine-winner.js
@@ -1,26 +1,38 @@
 // Create a 2D array to represent the board
-const board = []
 
 export const boardHandler = (tr) => {
+    const board = []
+    console.log(tr[0]);
+
     for (let i =0; i < tr.length; i++) {
         const row = tr[i] // is the cells in one row or column?
         const rowData = []
-        for (let j = 0; j < row.cells.length; j++) {
-            const cell = row[j]
-            rowData.push(cell.textContent)
+
+        for (let j = 0; j < row.children.length; j++) { //! row.children will key into the tr element's children, giving us the nest array to loop over.
+            const cell = row.children[j] //! this here is the actual td element that we can check what the mark is.
+
+            if (cell.dataset.player) { //! when we were building out the event listeners, I made it so when a player clicks on a cell, it adds the mark as accessible data in the element.
+                rowData.push(cell.dataset.player)
+            }
+            else {
+                rowData.push(' ');
+            }
         }
         board.push(rowData)
     }
+
+    console.log(board);
+
     return board
 }
-    
+
     // There are many possiblities for winning in tic tac toe.
     // How do we represent them all? -> iterate over all squares every time and see if there are three in a row?
 export const winnerHandler = (board => {
     for (let i = 0; i < 3; i++) {
         if (board[i][0] && board[i][0] === board[i][1] && board[i][0] === board[i][2]) {
             return board[i][0]
-    } 
+    }
     if (board[0][i] && board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
             return board[0][i]
         }
@@ -39,8 +51,7 @@ for (let i = 0; i < 3; i++) {
     for (let j =0; j < 3; j++) {
         if (board[i][j] === "") {
             return false
-        } else return h1.appendChild(spanTie) 
+        } else return h1.appendChild(spanTie)
     }
 }
 }
-
--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -1,4 +1,4 @@
-// // Your code here 
+// // Your code here
 import { winnerHandler, tieHandler, boardHandler } from "./determine-winner.js"
 
 
@@ -6,6 +6,8 @@ window.onload = () =>{
     // Code for adding the Xs and Os to the board
     const td = document.querySelectorAll("td")
     let currentPlayer = "X"
+    let currentTable = document.getElementsByTagName("tr")
+    let currentBoard = boardHandler(currentTable)
 
     td.forEach((el) => {
         const clickSquareHandler = (e) => {
@@ -16,16 +18,22 @@ window.onload = () =>{
                 if (currentPlayer === "X") {
                     img.setAttribute("src", "./images/X_image.png")
                     currentPlayer = "O"
+                    //! the code below adds an attribute we can access elsewhere, this is so we can determine wheter a cell is x or o
+                    e.target.setAttribute('data-player', 'x')
                 } else {
                     img.setAttribute("src", "./images/O_image.png")
                     currentPlayer = "X"
+                    //! the code below adds an attribute we can access elsewhere, this is so we can determine wheter a cell is x or o
+                    e.target.setAttribute('data-player', 'o')
                 }
                 e.target.appendChild(img)
             } else {
                     // do nothing
                     console.log("Square already is filled")
                 }
-            }
+            currentBoard = boardHandler(currentTable);
+            //! check for winner
+        }
         el.addEventListener("click", clickSquareHandler)
     })
 
@@ -33,7 +41,7 @@ window.onload = () =>{
 
     // We need to make conditionals to determine a winner or tie
     // We need to add an event to the header for win or tie
-    
+
     const h1 = document.getElementsByTagName("h1")
     // Add X winner tag/text to heading
     const spanWinnerX = document.createElement("span")
@@ -50,24 +58,24 @@ window.onload = () =>{
     spanTie.innerText = "Winner: Tie"
 
 
- const currentTable = document.getElementsByTagName("tr")
- const currentBoard = boardHandler(currentTable)
+
+
 
     // identify winner/tie
-    if (winnerHandler(currentBoard)) {
-        // if winner is X
-        if (currentBoard[i][j] === img && img.src === "./images/X_image.png") {
-            h1.appendChild(spanWinnerX)
-        }
-        // if winner is O
-        if (currentBoard[i][j] === img && img.src === "./images/O_image.png") {
-            h1.appendChild(spanWinnerO)
-        }
-    }
-    
-    // Add the code for tie
-        if (tieHandler(board)) {
-            h1.appendChild(spanTie)
-        }
+    // if (winnerHandler(currentBoard)) {
+    //     // if winner is X
+    //     if (currentBoard[i][j] === img && img.src === "./images/X_image.png") {
+    //         h1.appendChild(spanWinnerX)
+    //     }
+    //     // if winner is O
+    //     if (currentBoard[i][j] === img && img.src === "./images/O_image.png") {
+    //         h1.appendChild(spanWinnerO)
+    //     }
+    // }
+
+    // // Add the code for tie
+    //     if (tieHandler(board)) {
+    //         h1.appendChild(spanTie)
+    //     }
 
 }
